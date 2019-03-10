40fde6833b4e1f20e4dfe3db5ea6edf1c6467df9
Christian Palacios <crispal94@hotmail.com>
2019-03-10 00:35:19 -0500

    Cambio de librerias para ejecutar pdfs

--- config/snappy.php
+++ config/snappy.php
@@ -5,7 +5,7 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => '/usr/local/bin/wkhtmltopdf-amd64',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
